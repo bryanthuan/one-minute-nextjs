9e8cbef01d28337dcd0c11388705743c53565f3e
Brian Thuan <thuann89@gmail.com>
2018-08-22 19:32:07 +0700

    Adding several pages

--- components/page.js
+++ components/page.js
@@ -2,6 +2,16 @@ import Link from 'next/link'
 const Menu = ({ href, label }) => (
   <span>
     <Link href={href}><a>{label}</a></Link>
+    <style jsx>{`
+      {
+        padding: 6px 4px;
+      };
+      a {
+        color: #fff;
+        text-decoration: none;
+        font-size: 16px
+      }
+    `}</style>
   </span>
 )
 
---
+++ pages/comments.js
@@ -0,0 +1,66 @@
+import {Component} from 'react'
+import Link from 'next/link'
+import fetch from '../lib/fetch';
+import Page from '../components/page';
+
+const Comment = ({comment}) => (
+  <div className="comment">
+    <div dangerouslySetInnerHTML={
+      {
+        __html:comment.content
+      }
+    }></div>
+    <div>
+      <Link href={`/user?id=${comment.user}`}>
+        <a>{comment.user}</a>
+      </Link> | {comment.time_ago}
+    </div>
+    <style jsx>{`
+    .comment {
+      margin: 15px 0 15px 0;
+      border-top: 1px solid grey
+    }
+    `}</style>
+  </div>
+)
+
+const Comments = ({item})=> (
+  <div>
+    <h2>
+      <a href={item.url} target="_blank">{item.title}</a>
+    </h2>
+    <div>
+      <Link href={`/user?id=${item.user}`}>
+        <a>{item.user}</a>
+      </Link>
+      </div>
+      <span> | </span>
+      <span>{item.points} points</span>
+    <div>
+      {
+        item.comments.map(c=>(
+          <Comment key={c.id} comment={c} />
+        ))
+      }
+    </div>
+    <style jsx>{`
+      {
+        padding: 5px
+      }
+    `}</style>
+  </div>
+)
+export default class extends Component {
+  static async getInitialProps({query}){
+    return {
+      item: await fetch(`/item/${query.id}`)
+    }
+  }
+  render() {
+    return (
+      <Page>
+        <Comments item={this.props.item}/>
+      </Page>
+    )
+  }
+}
